f9744e1e8ec352d3ccc43d5ede1e6da42784d653
yejun <yejun0890@gmail.com>
2020-06-20 20:04:14 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/namu/intent01_2020020/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.namu.intent01_2020020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
